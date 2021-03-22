fb0a9bc4b782f6308c4121d65a23a208fed0c867
Markus Douglas Nascimento siqueira <makusdouglas@gmail.com>
2021-03-22 19:00:12 -0300

    changes background color

--- craco.config.js
+++ craco.config.js
@@ -7,7 +7,11 @@ module.exports = {
             options: {
                 lessLoaderOptions: {
                     lessOptions: {
-                        modifyVars: { '@primary-color': '#53284F', '@layout-header-background': '#231222' },
+                        modifyVars: {
+                            '@primary-color': '#53284F',
+                            '@layout-header-background': '#231222',
+                            '@menu-dark-inline-submenu-bg': '#10090F'
+                        },
                         javascriptEnabled: true,
                     },
                 },
